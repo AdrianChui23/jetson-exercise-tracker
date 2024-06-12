2ac7300e954da9783e8cdfd1c5c2db48483755c4
Adrian Chui <adrian.k.chui@gmail.com>
2024-06-12 15:23:12 -0700

    refactoring the key functions

--- main.py
+++ main.py
@@ -29,6 +29,24 @@ from jetson_inference import poseNet
 from jetson_utils import videoSource, videoOutput, Log, cudaFont
 from timeit import default_timer as timer
 
+
+def check_arm_visible(pose, shoulder_idx, elbow_idx, wrist_idx)->bool:
+    if shoulder_idx < 0 or elbow_idx < 0 or wrist_idx < 0:
+        return False
+    return True
+
+def check_arm_raised(pose, shoulder_idx, elbow_idx, wrist_idx)->bool:
+
+    if shoulder_idx < 0 or elbow_idx < 0 or wrist_idx < 0:
+        return False
+    
+    wrist = pose.Keypoints[wrist_idx]
+    shoulder = pose.Keypoints[left_shoulder_idx]
+
+    return True if (shoulder.y - wrist.y) > 0.0 else False
+
+
+
 # parse the command line
 parser = argparse.ArgumentParser(description="Run pose estimation DNN on a video/image stream.", 
                                  formatter_class=argparse.RawTextHelpFormatter, 
@@ -53,11 +71,11 @@ net = poseNet(args.network, sys.argv, args.threshold)
 # create video sources & outputs
 input = videoSource(args.input, argv=sys.argv)
 output = videoOutput(args.output, argv=sys.argv)
+
 font = cudaFont()
 arm_raised = False
 count_of_left_arm_movement = 0
-left_arm_pos = 0
-left_prev_arm_pos = 0
+
 
 # process frames until EOS or the user exits
 while True:
@@ -70,21 +88,12 @@ while True:
     # perform pose estimation (with overlay)
     poses = net.Process(img, overlay=args.overlay)
 
-    # print the pose results
-    print("detected {:d} objects in image".format(len(poses)))
-
     if len(poses) == 0:
         font.OverlayText(img, text=f"Body is not detected.",
                     x=5, y=5 + (font.GetSize()),
                     color=font.White, background=font.Gray40)
 
-
     for pose in poses:
-        print(pose)
-        print(pose.Keypoints)
-        print('Links', pose.Links)
-
-
         left_wrist_idx = pose.FindKeypoint('left_wrist')
         left_shoulder_idx = pose.FindKeypoint('left_shoulder')
         left_eye_idx = pose.FindKeypoint('left_eye')
@@ -104,29 +113,20 @@ while True:
         nose_idx = pose.FindKeypoint('nose')
 
 
-        if left_shoulder_idx < 0 or left_elbow_idx < 0 or left_wrist_idx < 0:
+
+        if not check_arm_visible(pose, shoulder_idx=left_shoulder_idx,
+                             elbow_idx=left_elbow_idx,  wrist_idx=left_wrist_idx):
             font.OverlayText(img, text=f"I can't see your whole left arm.",
-                    x=5, y=5 + (font.GetSize()),
-                    color=font.White, background=font.Gray40)
+                                x=5, y=5 + (font.GetSize()),
+                                color=font.White, background=font.Gray40)
             continue
-
-        left_prev_arm_pos = left_arm_pos
-        print(left_arm_pos, count_of_left_arm_movement)
-
-
-        left_wrist = pose.Keypoints[left_wrist_idx]
-        left_shoulder = pose.Keypoints[left_shoulder_idx]
-
-        left1_x = left_shoulder.x - left_wrist.x
-        left1_y = left_shoulder.y - left_wrist.y
-
-        if left_shoulder_idx > 0 and left_wrist_idx > 0:
-            if left1_y > 0:
-                if not arm_raised:
+        else:            
+            if check_arm_raised(pose, shoulder_idx=left_shoulder_idx,
+                                elbow_idx=left_elbow_idx,  wrist_idx=left_wrist_idx):
+                if not arm_raised :
                     start = timer()
                     arm_raised = True
-                    task_completed = False
-                
+                    task_completed = False                
             else:
                 arm_raised = False
                 font.OverlayText(img, text=f"Raise your arm please",
@@ -143,52 +143,12 @@ while True:
                         
                     task_completed = True
                 else:
-                    font.OverlayText(img, text=f"Lift the arm for { start +5 -timer()} seconds.",
+                    font.OverlayText(img, text=f"Lift the arm for { start +5 -timer(): .0f} seconds.",
                         x=5, y=50 + (font.GetSize()),
                         color=font.White, background=font.Gray40)
             font.OverlayText(img, text=f"arm is raised: {count_of_left_arm_movement}",
                     x=500, y=5 + (font.GetSize()),
                     color=font.White, background=font.Gray40)
-        else:
-            continue
-
-
-
-        right_wrist_idx = pose.FindKeypoint('right_wrist')
-        right_shoulder_idx = pose.FindKeypoint('right_shoulder')
-
-
-        if right_wrist_idx < 0 or right_shoulder_idx < 0:
-            continue
-
-
-        right_wrist = pose.Keypoints[right_wrist_idx]
-        right_shoulder = pose.Keypoints[right_shoulder_idx]
-
-        right1_x = right_shoulder.x - right_wrist.x
-        right1_y = right_shoulder.y - right_wrist.y
-
-        right_eye_idx = pose.FindKeypoint('right_eye')
-        right_ear_idx = pose.FindKeypoint('right_ear')
-
-        if right_eye_idx < 0 or right_ear_idx < 0:
-            continue
-
-
-        right_eye = pose.Keypoints[right_eye_idx]
-        right_ear = pose.Keypoints[right_ear_idx]
-
-        right2_x = right_eye.x - right_ear.x
-        right2_y = right_eye.y - right_ear.y
-
-        right02_x = right_ear.x
-        right002_x = right_eye.x
-        right02_y = right_ear.y
-        right002_y = right_eye.y
-
-
-
-
 
 
     # render the image
